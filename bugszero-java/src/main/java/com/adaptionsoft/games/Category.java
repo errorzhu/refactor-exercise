0c8ed22f2ce607c06d42111e2de472c762b332f1
982004587@qq.com <982004587@qq.com>
2020-02-26 11:02:49 +0800

    create category enum

---
+++ bugszero-java/src/main/java/com/adaptionsoft/games/Category.java
@@ -0,0 +1,12 @@
+package com.adaptionsoft.games;
+
+public enum Category {
+    pop("Pop"),science("Science"),sports("Sports"),rock("Rock");
+    private String name;
+
+    Category(String name){
+        this.name = name;
+
+    }
+    
+}
